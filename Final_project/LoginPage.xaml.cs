9455db38522a049c4fe9315896baef65eac0d958
F0sya <nikitachmyria@gmail.com>
2025-05-26 00:33:25 +0300

    Added LoginPage,RegisterPage and reworked MainWindow UI

---
+++ Final_project/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Final_project
+{
+    /// <summary>  
+    /// Interaction logic for LoginPage.xaml  
+    /// </summary>  
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Final_project/RegisterPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Final_project
+{
+    /// <summary>  
+    /// Interaction logic for LoginPage.xaml  
+    /// </summary>  
+    public partial class RegisterPage : Window
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
